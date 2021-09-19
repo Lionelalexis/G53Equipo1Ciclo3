1f82fe23637010473965e2d67352b16f7223b02b
Lionelalexis <90942826+Lionelalexis@users.noreply.github.com>
2021-09-18 20:07:16 -0500

    Proyecto practica

---
+++ tienda53/src/main/java/recursos/tienda53.sql
@@ -0,0 +1,35 @@
+######## Creacion y uso de la base de datos ########
+CREATE DATABASE tienda53grupo1;
+
+USE tienda53grupo1;
+
+######## modulo de login y usuarios ########
+CREATE TABLE usuarios (
+    cedula_usuario BIGINT PRIMARY KEY,
+    email_usuario VARCHAR(255) NOT NULL,
+    nombre_usuario VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    usuario VARCHAR(255) NOT NULL
+); 
+
+INSERT INTO usuarios VALUES(001,'alex@gmail.com','Alexander Leon','superuser','superuser');
+
+DROP TABLE usuarios;
+
+DELETE FROM usuarios 
+WHERE
+    cedula_usuario = 001;
+
+UPDATE usuarios 
+SET 
+    email_usuario = '',
+    nombre_usuario = '',
+    password = '',
+    usuario = ''
+WHERE
+    cedula_usuario = 001;
+
+SELECT 
+    *
+FROM
+    usuarios;
